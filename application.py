4c58ed013518bfb7e705b3d54375681cab0db8f4
Hardik888 <hardik89990@gmail.com>
2023-10-24 21:42:46 +0530

    application file

---
+++ application.py
@@ -0,0 +1,26 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+
+db = SQLAlchemy(app)
+
+
+class Drinks(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    description = db.Column(db.String(120))
+
+    def __repr__(self):
+        return f"{self.name} - {self.description}"
+
+
+@app.route('/')
+def home():
+    return "hello flask "
+
+
+@app.route('/drinks')
+def get_drinks():
+    return {"drinks": "drinks data"}
